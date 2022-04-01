23e8757caa3741c9b2dee6b2b29fec8adc25bfb1
camillepltr <camille.peltier04@gmail.com>
2022-04-01 15:59:10 +0200

    feat: added commit

--- src/common/utils/translate.tsx
+++ src/common/utils/translate.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
+// Translations are in lang/fr.json (Has to be a flat json, no nesting!)
+// id: the key of the translation in the json
+// Example of usage with parameter values :
+//  {translate('example.hello', {name: "Jean"})}
+//  With in the json :  { 'example.hello': 'Hello {name}}
 function translate(
   id: string | undefined,
   values?: Record<string, string> | undefined
