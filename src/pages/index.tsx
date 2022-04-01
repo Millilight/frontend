f2fe3985e7ce0cf82d5c5d3a8fa4c5f4df4bd28d
corentinbranchereau <branchereaucorentin@gmail.com>
2022-04-01 09:57:53 +0200

    refactor: define all url in config.ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import { Typography } from '@mui/material';
 // import { PageCountriesComp } from 'generated/page';
 import { withApollo } from '@/utils/withApollo';
-
 import { useRouter } from 'next/router';
 // import { Visibility, VisibilityOff } from '@mui/icons-material';
 import Signup from '@/components/Signup/Signup';
 import Signin from '@/components/Signin/Signin';
-import translate from '@/utils/translate';
-
 import translate from '@/utils/translate';
 
 const Index = () => {
