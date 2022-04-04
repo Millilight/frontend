f4e8d0fcd122a28fd14bc72db7a3713f833bb1de
camillepltr <camille.peltier04@gmail.com>
2022-04-04 09:56:15 +0200

    fix : added translation for coming soon page + redirection to a new page for each category

--- src/pages/espace-personnel/demarches-administratives.tsx
+++ src/pages/espace-personnel/demarches-administratives.tsx
@@ -1,18 +1,20 @@
 import { withApollo } from '@/utils/withApollo';
 import { Box } from '@mui/material';
 import MenuDrawer from '@/components/MenuDrawer/MenuDrawer';
-import PersonalSpaceHome from '@/components/PersonalSpaceHome/PersonalSpaceHome';
+import Paperwork from '@/components/Paperwork/Paperwork';
 
-const PersonalSpace = () => {
-  const selectedPage = 'home';
+const PaperworkPage = () => {
+  //To know what to highlight on the menu
+  const selectedPage = 'paperwork';
+
   return (
     <div className="flex-container">
       <MenuDrawer selectedPage={selectedPage} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <PersonalSpaceHome />
+        <Paperwork />
       </Box>
     </div>
   );
 };
 
-export default withApollo(PersonalSpace);
+export default withApollo(PaperworkPage);
--- src/pages/espace-personnel/volontes-medicales.tsx
+++ src/pages/espace-personnel/volontes-medicales.tsx
@@ -1,18 +1,20 @@
 import { withApollo } from '@/utils/withApollo';
 import { Box } from '@mui/material';
 import MenuDrawer from '@/components/MenuDrawer/MenuDrawer';
-import PersonalSpaceHome from '@/components/PersonalSpaceHome/PersonalSpaceHome';
+import Medical from '@/components/Medical/Medical';
 
-const PersonalSpace = () => {
-  const selectedPage = 'home';
+const MedicalWishesPage = () => {
+  //To know what to highlight on the menu
+  const selectedPage = 'medical';
+
   return (
     <div className="flex-container">
       <MenuDrawer selectedPage={selectedPage} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <PersonalSpaceHome />
+        <Medical />
       </Box>
     </div>
   );
 };
 
-export default withApollo(PersonalSpace);
+export default withApollo(MedicalWishesPage);
