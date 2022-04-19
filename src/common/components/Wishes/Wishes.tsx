2232a3980d1d7e576a7d350793e14d8689005496
corentinbranchereau <branchereaucorentin@gmail.com>
2022-04-19 23:40:17 +0200

    fix: scroll on ceremonial showing help

--- src/common/components/Wishes/Wishes.tsx
+++ src/common/components/Wishes/Wishes.tsx
@@ -19,44 +19,46 @@ export default function Wishes(props: {
   ) => void;
 }) {
   return (
-    <Stack spacing={2} justifyContent="center" alignItems="center">
-      {props.wishes
-        ? props.wishes.map((wish: Wish) => (
-            <Box
-              key={wish.wishId}
-              onClick={() =>
-                props.helpCallback(wish.title, wish.image, wish.help)
-              }
-              sx={{
-                width: '80%',
-                boxShadow: 2,
-              }}
-            >
-              <Accordion>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls="panel1a-content"
-                  id="panel1a-header"
-                  sx={{
-                    paddingX: '50px',
-                    paddingY: '10px',
-                  }}
-                >
-                  <Typography>{wish.title}</Typography>
-                </AccordionSummary>
-                <AccordionDetails
-                  sx={{
-                    paddingX: '50px',
-                    paddingBottom: '20px',
-                    paddingTop: '0px',
-                  }}
-                >
-                  <WishContent wish={wish}></WishContent>
-                </AccordionDetails>
-              </Accordion>
-            </Box>
-          ))
-        : null}
-    </Stack>
+    <div className="wish-content-container">
+      <Stack spacing={2} justifyContent="center" alignItems="center">
+        {props.wishes
+          ? props.wishes.map((wish: Wish) => (
+              <Box
+                key={wish.wishId}
+                onClick={() =>
+                  props.helpCallback(wish.title, wish.image, wish.help)
+                }
+                sx={{
+                  width: '80%',
+                  boxShadow: 2,
+                }}
+              >
+                <Accordion>
+                  <AccordionSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    sx={{
+                      paddingX: '50px',
+                      paddingY: '10px',
+                    }}
+                  >
+                    <Typography>{wish.title}</Typography>
+                  </AccordionSummary>
+                  <AccordionDetails
+                    sx={{
+                      paddingX: '50px',
+                      paddingBottom: '20px',
+                      paddingTop: '0px',
+                    }}
+                  >
+                    <WishContent wish={wish}></WishContent>
+                  </AccordionDetails>
+                </Accordion>
+              </Box>
+            ))
+          : null}
+      </Stack>
+    </div>
   );
 }
