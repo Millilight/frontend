eaa2c6f47921e092eb44ce562663c0ab0f78d4a5
camillepltr <camille.peltier04@gmail.com>
2022-04-19 15:43:03 +0200

    fixes related to my account page

--- src/pages/espace-personnel/mon-compte.tsx
+++ src/pages/espace-personnel/mon-compte.tsx
@@ -1,10 +1,12 @@
 import { withApollo } from '@/utils/withApollo';
 import MenuDrawer from '@/components/MenuDrawer/MenuDrawer';
 import MyAccount from '@/components/MyAccount/MyAccount';
+import AccountMenu from '@/components/AccountMenu/AccountMenu';
 
 const MyAccountPage = () => {
   return (
     <div className="">
+      <AccountMenu />
       <MenuDrawer selectedPage={''} />
       <main>
         <MyAccount />
